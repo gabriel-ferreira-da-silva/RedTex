d037ddb5362eedb2f0ac534ef720986983cbb941
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2025-04-14 21:56:09 -0300

    login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage /> } />
         <Route path="/login" element={<LoginPage /> }/>
+        <Route path="/register" element={<RegisterPage /> }/>
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterMainPanel from "../components/RegisterComponents/RegisterMainPanel";
+
+function RegisterPage() {
+  return (
+    <div>
+      <RegisterMainPanel/>
+    </div>
+  );
+}
+
+export default RegisterPage;
